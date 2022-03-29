cd1017939dcdb9174f4b6a3d94ac0408a901f0fc
桐桐鸭 <99520853+tty2007@users.noreply.github.com>
2022-03-29 21:34:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ let counter = 0;
 c2.width = c3.width = window.innerWidth;
 c2.height = c3.height = window.innerHeight;
 ctx1.fillStyle = '#000';
-const text = '我是你爹';
+const text = '刘昱乔宝贝';
 let textWidth = 99999999;
 
 while (textWidth > window.innerWidth) {
